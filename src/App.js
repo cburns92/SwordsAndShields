19e1874e790698a74b15410dc0ec85ff8d58c96c
Colin <cburns81592@gmail.com>
2018-12-17 19:45:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Typed from 'react-typed';
+import pyramid from './images/pyramid.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <nav className="navbar">
+        <row>
+          <div class="col-4">
+            <a class = "nav-link" href=".">
+                <Typed 
+                    strings={['[Character]']} 
+                    typeSpeed={40} 
+                    showCursor={false}
+                    startDelay={15000}
+                />
+            </a>
+            </div>
+            <div class="col-4">
+            <a class = "nav-link" href=".">
+            <Typed 
+                    strings={['[Arena]']} 
+                    typeSpeed={40}
+                    showCursor={false} 
+                    startDelay={16000}
+                />
+            </a>
+            </div>
+            <div class="col-4">
+            <a class = "nav-link" href=".">
+            <Typed 
+                    strings={['[Rankings]']} 
+                    typeSpeed={40}
+                    showCursor={false} 
+                    startDelay={17000}
+                />
+            </a>
+            </div>
+        </row>
+        </nav>
         </header>
+        <div class = "main">
+          <h1 className="title">
+                <Typed 
+                    strings={["Swords and Shields"]} 
+                    typeSpeed={200}
+                    startDelay={0}
+                    showCursor={true} 
+                />
+                </h1>
+          <img src={pyramid} className = 'pyramid' alt = 'pyramid'></img>
+          <h2>
+                <Typed 
+                    strings={['Welcome Adventurer, it is time to begin your journey!']} 
+                    typeSpeed={75}
+                    startDelay={6000}
+                    showCursor={false} 
+                />
+          </h2>
+          <a class = "nav-link" href=".">
+            <Typed 
+                    strings={['[Create A Character]']} 
+                    typeSpeed={75}
+                    showCursor={false} 
+                    startDelay={12000}
+            />
+          </a>
+        </div>
+
+          
+
+          
       </div>
     );
   }
