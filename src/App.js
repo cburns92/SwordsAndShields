178b65fcfaf14252653ae35d1756bfbe75a4f87b
Colin <cburns81592@gmail.com>
2018-12-17 22:29:36 -0500

    React Router Template and Placeholders, fixed menu refresh bug with ternary operator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './components/Home/Home'
-import Navigation from './components/Navigation/Navigation'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Typed from 'react-typed';
+
+const Character = () => <h1>This is the Character Sheet Page!</h1>
+const Arena = () => <h1>This is the Arena! Or Arena Lobby!</h1>
+const Rankings = () => <h1>This is the Player Rankings Page!</h1>
 
 class App extends Component {
+  
+  state = {
+    location: window.location.href
+  };
+
   render() {
     return (
+      <Router>
       <div className="App">
-      <Navigation/>  
-      <Home/>
+      <header className="App-header">
+      
+          <nav className="navbar">
+            <row>
+              <div class="col-4">
+                <Link class = "nav-link" to = "/character">
+                    <Typed 
+                        strings={['[Character]']} 
+                        typeSpeed={40} 
+                        showCursor={false}
+                        startDelay={this.state.location !== "http://localhost:3000/" ? 0 : 15000}
+                    />
+                </Link>
+              </div>
+
+              <div class="col-4">
+                <Link class = "nav-link" to = "/arena">
+                <Typed 
+                        strings={['[Arena]']} 
+                        typeSpeed={40}
+                        showCursor={false} 
+                        startDelay={this.state.location !== "http://localhost:3000/" ? 0 : 16000}
+                    />
+                </Link>
+              </div>
+
+              <div class="col-4">
+                <Link class = "nav-link" to = "/rankings">
+                <Typed 
+                        strings={['[Rankings]']} 
+                        typeSpeed={40}
+                        showCursor={false} 
+                        startDelay={this.state.location !== "http://localhost:3000/" ? 0 : 17000}
+                    />
+                </Link>
+              </div>
+            </row>
+          </nav>
+          
+        </header>
+
+        <Route exact path = "/" component = {Home} />
+        <Route path = "/character" component = {Character} />
+        <Route path = "/arena" component = {Arena} />
+        <Route path = "/rankings" component = {Rankings} />
       </div>
+      
+      </Router>
     );
   }
 }
